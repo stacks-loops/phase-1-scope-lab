fe94fa19f12ec3c359022b51a4c03a6cc99ecb95
Parker <p.cronin.connolly@gmail.com>
2023-12-11 19:11:21 -0500

    scope lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    console.log(customerName)
+}
+
+//upperCaseCustomerName()
+//console.log(customerName)
+
+function setBestCustomer() {
+    bestCustomer = window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'cats'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = leastFavoriteCustomer.toUpperCase()
+
+}
